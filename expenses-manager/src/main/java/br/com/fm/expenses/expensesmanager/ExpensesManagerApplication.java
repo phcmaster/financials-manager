a294467f1d4563d03123e4d60ead42015090ee93
Pedro Henrique <pedro.hco@ibm.com>
2020-10-20 15:24:15 -0300

    adding expenses-manager

---
+++ expenses-manager/src/main/java/br/com/fm/expenses/expensesmanager/ExpensesManagerApplication.java
@@ -0,0 +1,13 @@
+package br.com.fm.expenses.expensesmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensesManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensesManagerApplication.class, args);
+	}
+
+}
---
+++ expenses-manager/src/test/java/br/com/fm/expenses/expensesmanager/ExpensesManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fm.expenses.expensesmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensesManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
