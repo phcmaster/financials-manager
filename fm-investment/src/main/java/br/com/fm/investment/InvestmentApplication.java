d0b6551c9173133494e639de678f5c4d2a6e5895
Pedro Henrique <pedro.hco@ibm.com>
2020-11-04 20:24:59 -0300

    front end fisrt commit

--- fm-investment/src/main/java/br/com/fm/investment/InvestmentApplication.java
+++ fm-investment/src/main/java/br/com/fm/investment/InvestmentApplication.java
@@ -2,8 +2,10 @@ package br.com.fm.investment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class InvestmentApplication {
 
 	public static void main(String[] args) {
--- fm-investment/src/main/java/br/com/fm/investment/service/feign/FeignClientCurrencyQuote.java
+++ fm-investment/src/main/java/br/com/fm/investment/service/feign/FeignClientCurrencyQuote.java
@@ -1,4 +1,7 @@
 package br.com.fm.investment.service.feign;
 
+import org.springframework.cloud.openfeign.FeignClient;
+
+@FeignClient(name = "", url = "")
 public interface FeignClientCurrencyQuote {
 }
